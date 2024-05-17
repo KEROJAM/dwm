0668bd3ad34ce35056986846f75eedf18a96eb3a
KEROJAM <95250477+KEROJAM@users.noreply.github.com>
2024-05-17 14:29:07 -0600

    Update config.h

--- config.h
+++ config.h
@@ -16,8 +16,8 @@ static const char *fonts[]          = { "mononoki Nerd Font:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray3[]       = "#ffffff";
+static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#ca7666";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
