8e310f0763dd295870a5796565db994d756c6dc3
KEROJAM <majorekcs@hotmail.com>
2025-02-26 11:32:49 -0600

    font change 2

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 0;       /*vertical padding of bar*/
 static const int sidepad            = 0;       /*horizontal padding of bar*/
-static const char *fonts[]          = { "azukifontLB:size=15" };
+static const char *fonts[]          = { "azukifont:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#fdf5ed";
 static const char col_gray2[]       = "#444444";
