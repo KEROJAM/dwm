a6f5559b8a8fe23f7567c7d6309734ebfb1e5d9f
KEROJAM <majorekcs@hotmail.com>
2025-04-26 20:16:29 -0600

    changed gaps

--- config.h
+++ config.h
@@ -3,15 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 3;/* horiz inner gap between windows */
-static const unsigned int gappiv    = 2;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 2;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 2;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 4;/* horiz inner gap between windows */
+static const unsigned int gappiv    = 4;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 4;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 4;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 2;       /*vertical padding of bar*/
-static const int sidepad            = 2;       /*horizontal padding of bar*/
+static const int vertpad            = 4;       /*vertical padding of bar*/
+static const int sidepad            = 4;       /*horizontal padding of bar*/
 static const char *fonts[]          = { "Mononoki nerd font:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#fdf5ed";
